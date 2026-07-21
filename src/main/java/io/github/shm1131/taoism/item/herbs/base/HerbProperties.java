6b1cffa8f6fd2f0c545847cfb72d719ba84aa586
shm1131 <3435294706@qq.com>
2026-07-21 22:17:24 +0800

    4

---
+++ src/main/java/io/github/shm1131/taoism/item/herbs/base/HerbProperties.java
@@ -0,0 +1,8 @@
+package io.github.shm1131.taoism.item.herbs.base;
+
+public record HerbProperties(
+        Flavor flavor,
+        Nature nature,
+        float toxicity,
+        float potency
+) {}
---
+++ src/main/java/io/github/shm1131/taoism/item/herbs/dang_gui/DangGui.java
@@ -0,0 +1,10 @@
+package io.github.shm1131.taoism.item.herbs.dang_gui;
+
+import io.github.shm1131.taoism.item.herbs.base.BaseHerbItem;
+import io.github.shm1131.taoism.item.herbs.base.HerbProperties;
+
+public class DangGui extends BaseHerbItem {
+    public DangGui(Properties properties, HerbProperties herbProps) {
+        super(properties, herbProps);
+    }
+}
